e50eeefdd557d94e07fb0713cb2766c4de67f490
Danilo Carvalho <danilogga@gmail.com>
2019-08-23 09:18:57 -0400

    commit 2 no branch ITAU

--- sendEmail/src/carvalho/danilo/SendEmail.java
+++ sendEmail/src/carvalho/danilo/SendEmail.java
@@ -22,6 +22,7 @@ public class SendEmail {
         try {
             System.out.println("ATUALIZANDO BRANCH ITAU git");
             sendEmail();
+            System.out.println("NOVA ALTERAÇÃO NO BRANCH ITAU");
 
             System.out.println(new String(FileUtility.getBytesOfFile(new File("email-template.html")), Charset.forName("UTF-8")));
 
